10ae84782fb3fe1ddc9c84e7bf164d5ba7850673
Jose Saavedra <jmsaavedrar@gmail.com>
2020-03-11 14:12:16 -0300

    perceptron added

---
+++ perceptron/example_view_data.py
@@ -0,0 +1,55 @@
+'''
+Created on Mar 4, 2019
+
+@author: jsaavedr
+This is an example of plotting iris data
+'''
+
+from sklearn.datasets import load_iris
+import matplotlib.pyplot as plt
+import numpy as np
+
+def logsig(x):
+    """
+    sigmoide function
+    """
+    return 1.0 / ( 1.0 + np.exp( - x ))
+
+def tanh(x):    
+    return (np.exp(x )- np.exp(-x)) / (np.exp(x) + np.exp(-x))
+
+def dtanh(x):    
+    a = (np.exp(x )- np.exp(-x)) / (np.exp(x) + np.exp(-x))
+    return 1- a*a
+
+def dlogsig(x):
+    return np.exp( - x ) / (( 1.0 + np.exp( - x ))**2)
+
+def loadIrisData():
+    iris = load_iris()
+    data = iris.data[:,[0,3]]
+    data = data[0:100]
+    target = iris.target[0:100]    
+    #data for class 0 
+    data_0 = data[:50,:]
+    #data for class 1      
+    data_1 = data[50:,:]    
+    fig, x = plt.subplots(1)
+    x.set_xlabel("petal length")
+    x.set_ylabel("sepal width")
+    x.plot(data_0[:,0], data_0[:,1], 'r+' )    
+    x.plot(data_1[:,0], data_1[:,1], 'bo' )
+    x.plot()
+    plt.show()
+
+def plotFunction(fun):
+    x = np.arange(-1,1,0.01)
+    print(x)
+    y = fun(x)
+    fig, gx = plt.subplots(1)
+    gx.plot(x,y, 'bo')
+    plt.show()
+      
+if __name__ == "__main__" :             
+    loadIrisData()
+    #plotFunction(dlogsig)
---
+++ perceptron/perceptron.py
@@ -0,0 +1,48 @@
+'''
+Created on Feb 28, 2019
+
+@author: jsaavedr
+
+A simple implementation of a logistic regression (Perceptron)
+'''
+import numpy as np
+
+def logsig(x):
+    """
+    sigmoide function
+    """
+    return 1.0 / ( 1.0 + np.exp( - x ))
+
+
+def train(data, target, number_of_iterations, lr=0.01):
+    """
+    training a simple perceptron using  the gradient descent algorithm
+    """    
+    n, d = data.shape
+    assert n == target.size, "target size incompatible with data size"
+    
+    ones = np.ones((n,1))
+    data = np.append(ones, data, axis = 1)
+    d = d + 1    
+    w = np.random.rand(d)
+    for it in range(number_of_iterations) :
+        v = np.matmul(data,w)
+        y = logsig(v)        
+        #using mse loss
+        loss = np.mean(0.5 * ((target - y) ** 2))      
+        dif =  np.mean((y - target) * (y * (1.0 - y)) * np.transpose(data) , axis = 1)
+        #using Cross-Entropy loss        
+        #loss = - np.mean((target * np.log(y) + (1 - target) * np.log(1 - y)))
+        #dif =   np.mean( (y - target) * np.transpose(data) , axis = 1)
+        w = w - lr * dif
+        if it % 100 == 0 :          
+            print("loss ({}): {}".format(it, loss))
+    return w
+    
+def predict(data, w): 
+    n = data.shape[0]   
+    ones = np.ones((n,1))
+    data = np.append(ones, data, axis = 1)
+    return logsig(np.matmul(data, w))
+    
+    
